2009158cac0ba295e2b7e0daec059b5325abaa85
Katiacrisci <katcrisci@gmail.com>
2023-11-21 16:38:39 +0100

    add deposit and withdraw

--- conti.ts
+++ conti.ts
@@ -1,15 +1,27 @@
-class sonAccount {
-    balanceInit: number = 0; 
-    constructor( _balance: number) {
-        this.balanceInit = _balance;
-    }
+class MotherAccount {
+  balanceInit: number = 0;
 
+  constructor(_balance: number) {
+    this.balanceInit = _balance;
+  }
+
+  oneDeposit(amount: number): number {
+    this.balanceInit += amount;
+    return this.balanceInit;
+  }
+
+  oneWithDraw(amount: number): number {
+    this.balanceInit -= amount;
+    return this.balanceInit;
+  }
 }
 
-class motherAccount {
-    balanceInit: number = 0; 
-    constructor( _balance: number) {
-        this.balancInit = _balance;
-    }
+class SonAccount extends MotherAccount {
+  constructor(_balance: number) {
+    super(_balance);
+  }
 }
 
+const son = new SonAccount(1000);
+son.balanceInit;
+console.log(son.oneWithDraw(100));
