8d7743ceba28dda4b37205482b0337bb5d3929d1
Katiacrisci <katcrisci@gmail.com>
2023-11-21 16:24:32 +0100

    first commit

---
+++ conti.ts
@@ -0,0 +1,15 @@
+class sonAccount {
+    balanceInit: number = 0; 
+    constructor( _balance: number) {
+        this.balanceInit = _balance;
+    }
+
+}
+
+class motherAccount {
+    balanceInit: number = 0; 
+    constructor( _balance: number) {
+        this.balancInit = _balance;
+    }
+}
+
